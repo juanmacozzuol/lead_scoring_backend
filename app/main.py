5e9d9ebfe0ccb9c15e4ab8a531425ca8c2f98a2f
angie <angelesmendozafarias@gmail.com>
2025-11-06 23:01:28 -0300

    agregue cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session   
 from sqlalchemy import text          
 from .database.database import get_db      
@@ -7,6 +8,21 @@ from app.routers import routes
 
 # 1. Instancia de la aplicación FastAPI 
 app = FastAPI()
+
+# --- Configuración de CORS ---
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+         "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 app.include_router(routes.router)
 def read_root():
     # 3. La función de ruta devuelve el dato que se convertirá a JSON
